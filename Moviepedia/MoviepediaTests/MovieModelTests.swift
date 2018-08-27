94c61ef82b2f3acd85cc3b7609991ec8ff69270a
Lucas Ferraço <lucasferracof@gmail.com>
2018-08-26 21:05:28 -0300

    Adding Movie's Unit Test

---
+++ Moviepedia/MoviepediaTests/MovieModelTests.swift
@@ -0,0 +1,57 @@
+//
+//  MovieModelTests.swift
+//  MovieModelTests
+//
+//  Created by Lucas Ferraço on 26/08/18.
+//  Copyright © 2018 Lucas Ferraço. All rights reserved.
+//
+
+import XCTest
+import Foundation
+
+class MovieModelTests: XCTestCase {
+	
+    func testMovieDecoding() {
+        let json = """
+		{
+		    "vote_count": 0,
+		    "id": 439079,
+		    "video": false,
+		    "vote_average": 0,
+		    "title": "The Nun",
+		    "popularity": 20.592,
+		    "poster_path": "/sFC1ElvoKGdHJIWRpNB3xWJ9lJA.jpg",
+		    "original_language": "en",
+		    "original_title": "The Nun",
+		    "genre_ids": [
+		        27,
+		        9648,
+		        53
+		    ],
+		    "backdrop_path": "/tfKxFPq4lel0bL80H0C6kGYKGEq.jpg",
+		    "adult": false,
+		    "overview": "When a young nun at a cloistered abbey in Romania takes her own life, a priest with a haunted past and a novitiate on the threshold of her final vows are sent by the Vatican to investigate. Together they uncover the order’s unholy secret. Risking not only their lives but their faith and their very souls, they confront a malevolent force in the form of the same demonic nun that first terrorized audiences in “The Conjuring 2,” as the abbey becomes a horrific battleground between the living and the damned.",
+		    "release_date": "2018-09-07"
+		}
+		""".data(using: .utf8)!
+		
+		do {
+			let movie = try JSONDecoder().decode(Movie.self, from: json)
+			
+			XCTAssertEqual(movie.id, 439079)
+			XCTAssertEqual(movie.title, "The Nun")
+			XCTAssertEqual(movie.overview, "When a young nun at a cloistered abbey in Romania takes her own life, a priest with a haunted past and a novitiate on the threshold of her final vows are sent by the Vatican to investigate. Together they uncover the order’s unholy secret. Risking not only their lives but their faith and their very souls, they confront a malevolent force in the form of the same demonic nun that first terrorized audiences in “The Conjuring 2,” as the abbey becomes a horrific battleground between the living and the damned.")
+			XCTAssertEqual(movie.genreIds, [27, 9648, 53])
+			
+			let formatter = DateFormatter.yyyyMMddFormat
+			XCTAssertEqual(movie.releaseDate, formatter.date(from: "2018-09-07"))
+			
+			XCTAssertEqual(movie.originalTitle, "The Nun")
+			XCTAssertEqual(movie.originalLanguage, "en")
+			XCTAssertEqual(movie.posterPath, "/sFC1ElvoKGdHJIWRpNB3xWJ9lJA.jpg")
+			XCTAssertEqual(movie.backdropPath, "/tfKxFPq4lel0bL80H0C6kGYKGEq.jpg")
+		} catch _ {
+			XCTFail()
+		}
+    }
+}
